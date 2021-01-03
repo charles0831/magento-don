43b8e7ca84e2caeff309a6ba6f684b28142ce2d4
akio <akioUnity@outlook.com>
2021-01-03 22:46:03 +0700

    add custom ship module

---
+++ app/code/Akio/CustomShipping/registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amgPC
+ * Date: 2020.12.26
+ * Time: 9:09 下午
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Akio_CustomShipping',
+    __DIR__
+);
+
